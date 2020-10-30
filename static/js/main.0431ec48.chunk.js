0dca8848f3dd24c3b0793288cd1da124da4897d0
your name here <echen831@gmail.com>
2020-10-30 14:38:40 -0700

    Updates

---
+++ static/js/main.0431ec48.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptandem_quiz_app=this.webpackJsonptandem_quiz_app||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(0),r=n(1),i=n.n(r),s=n(3),a=n.n(s);n(9);var d=function(){return Object(c.jsx)("div",{className:"App",children:Object(c.jsx)("h1",{children:"Hello World"})})};a.a.render(Object(c.jsx)(i.a.StrictMode,{children:Object(c.jsx)(d,{})}),document.getElementById("root"))},9:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.0431ec48.chunk.js.map
