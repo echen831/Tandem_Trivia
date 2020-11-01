9dc1821046e1c64adbed675e7f6f9824fb9c3506
your name here <echen831@gmail.com>
2020-11-01 11:52:50 -0800

    update splash page

--- src/components/splash.jsx
+++ src/components/splash.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-import Img1 from "../assets/pic_1.png";
-import Img2 from "../assets/pic_2.png";
-import Img3 from "../assets/pic_3.png";
-import Img4 from "../assets/pic_4.png";
+import Intro from '../intro.gif';
+
 
 export const Splash = () => {
 
@@ -17,28 +15,29 @@ export const Splash = () => {
                         <p className='close-btn' onClick={() => setShow(false)}>&times;</p>
                     </div>
                 </div>
-                <div>
-                    <div className='image-container'>
-                        <img src={Img1} alt=""/>
-                        <p>You will get 10 random multiple choice questions</p>
+                <div className='instruction-container'>
+                    <div className='instructions'>
+                        <h3>Instructions: </h3>
+                        <ul>
+                            <p>{"1) You will get 10 random multiple choice trivia questions."}</p>
+                            <p>{"2) Select the best answer out of 4 choices."}</p>
+                            <p>{"3) Press submit when you are ready."}</p>
+                            <p>{"4) You will see the correct answer after you submit."}</p>
+                            <p>{"5) See how many can you answer correctly!"}</p>
+                        </ul>
+                        <p className='submit-btn' onClick={() => setShow(false)}>Play Now!</p>
                     </div>
-                    <div className='image-container'>
-                        <p>Choose the correct answer out of 4 possible choices</p>
-                        <img src={Img2} alt=""/>
-                    </div>
-                    <div className='image-container'>
-
-                        <img src={Img3} alt="" />
-                        <p>Submit your answer</p>
-                    </div>
-                    <div className='image-container'>
-                        <p>10 points per correct answer</p>
-                        <img src={Img4} alt="" />
+                    <div className='intro'>
+                        <img src={Intro} alt=""/>
                     </div>
                 </div>
                 <div className='splash-footer'>
                         <p>Created by: Eric Chen</p>
-                        <p>Please check out my portfolio <a href="https://echen831.github.io/Eric-Chen/" target='_blank'>here!</a></p>
+                        <div className='footer-links'>
+                            <a href="https://github.com/echen831" target='_blank'>Github</a>
+                            <a href="https://www.linkedin.com/in/eric-chen-782b951a9/" target='_blank'>LinkedIn</a>
+                            <a href="https://echen831.github.io/Eric-Chen/" target='_blank'>Portfolio</a>
+                        </div>
                 </div>
             </div>
 
